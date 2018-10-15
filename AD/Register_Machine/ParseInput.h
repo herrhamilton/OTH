cc2223968fc8affea7a6b08cac7a1411e23adfbd
joshamil <jouhou@posteo.de>
2018-10-15 21:59:26 +0200

    removed unnecessary files

--- AD/Register_Machine/ParseInput.h
+++
@@ -1,8 +0,0 @@
-#ifndef PARSE_INPUT_H
-#define PARSE_INPUT_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include "Commands.h"
-void parseInput(char *fileName, struct command** command_ptr, int* commandCount, const int MAX_COMMANDS);
-#endif // PARSE_INPUT_H
